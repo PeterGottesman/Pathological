5411ecec9fea15afe48a1953e32880838c6891f4
reagan <rjgron01@louisville.edu>
2025-11-10 21:53:24 +0000

    Fixed clang format for obj_loader_test

--- tests/obj_loader_test.cpp
+++ tests/obj_loader_test.cpp
@@ -35,59 +35,61 @@ bool CheckVec3Expected(Vec3 a, float i, float j, float k, std::string on_fail)
 	return succeed;
 }
 
-bool test(){
-    std::string testdata = "v 0.0 0.0 0.0\n"
-    "v 0.0 0.0 1.0\n"
-    "v 0.0 1.0 0.0\n"
-    "vn 0.0 0.0 1.0\n"
-    "vn 1.0 0.0 1.0\n"
-    "vn 0.0 1.0 1.0\n"
-    "f 1//1 2//2 3//3\n";
+bool test()
+{
+	std::string testdata = "v 0.0 0.0 0.0\n"
+	                       "v 0.0 0.0 1.0\n"
+	                       "v 0.0 1.0 0.0\n"
+	                       "vn 0.0 0.0 1.0\n"
+	                       "vn 1.0 0.0 1.0\n"
+	                       "vn 0.0 1.0 1.0\n"
+	                       "f 1//1 2//2 3//3\n";
 
-    istringstream test_data_stream(testdata);
+	istringstream test_data_stream(testdata);
 
-    ObjData output = load_obj_stream(test_data_stream);
+	ObjData output = load_obj_stream(test_data_stream);
 
-    //test output of load_obj_stream
-    //Vec3 v matches defined in test data (correct # of elements)
-    //check ind. element to see if match in test data
+	// test output of load_obj_stream
+	// Vec3 v matches defined in test data (correct # of elements)
+	// check ind. element to see if match in test data
 
-    bool succeed = true;
+	bool succeed = true;
 
-    if(output.V.size()!=3){
-        std::cerr << "Error: Face must have exactly 3 vertices";
-        succeed = false;
-    }
+	if (output.V.size() != 3)
+	{
+		std::cerr << "Error: Face must have exactly 3 vertices";
+		succeed = false;
+	}
 
-    if(output.VN.size()!=3){
-        std::cerr << "Error: Face must have exactly 3 vertex normals";
-        succeed = false;
-    }
+	if (output.VN.size() != 3)
+	{
+		std::cerr << "Error: Face must have exactly 3 vertex normals";
+		succeed = false;
+	}
 
-    if(output.tris.size()!=1){
-        std::cerr << "Error: Face must have exactly one tris";
-        succeed = false;
-    }
-    const auto& tris0 = output.tris[0];
-    CheckVec3Expected(output.V[0], 0, 0, 0, "Failed output.v is incorrect");
-    CheckVec3Expected(output.V[1], 0, 0, 1, "Failed output.v is incorrect");
-    CheckVec3Expected(output.V[2], 0, 1, 0, "Failed output.v is incorrect");
+	if (output.tris.size() != 1)
+	{
+		std::cerr << "Error: Face must have exactly one tris";
+		succeed = false;
+	}
+	const auto& tris0 = output.tris[0];
+	CheckVec3Expected(output.V[0], 0, 0, 0, "Failed output.v is incorrect");
+	CheckVec3Expected(output.V[1], 0, 0, 1, "Failed output.v is incorrect");
+	CheckVec3Expected(output.V[2], 0, 1, 0, "Failed output.v is incorrect");
 
-    CheckVec3Expected(output.VN[0], 0, 0, 1, "Failed output.vn is incorrect");
-    CheckVec3Expected(output.VN[1], 1, 0, 1, "Failed output.vn is incorrect");
-    CheckVec3Expected(output.VN[2], 0, 1, 1, "Failed output.vn is incorrect");
+	CheckVec3Expected(output.VN[0], 0, 0, 1, "Failed output.vn is incorrect");
+	CheckVec3Expected(output.VN[1], 1, 0, 1, "Failed output.vn is incorrect");
+	CheckVec3Expected(output.VN[2], 0, 1, 1, "Failed output.vn is incorrect");
 
-    //refer to line 37 for formating on tris coordinates
-    CheckVec3Expected(output.V[tris0.v[0]], 0,0,0, "Tri[0] v[0] points to wrong vertex");
-    CheckVec3Expected(output.V[tris0.v[1]], 0,0,1, "Tri[0] v[1] points to wrong vertex");
-    CheckVec3Expected(output.V[tris0.v[2]], 0,1,0, "Tri[0] v[2] points to wrong vertex");
-    CheckVec3Expected(output.VN[tris0.vn[0]], 0,0,1, "Tri[0] vn[0] points to wrong vertex normal");
-    CheckVec3Expected(output.VN[tris0.vn[1]], 1,0,1, "Tri[0] vn[1] points to wrong vertex normal");
-    CheckVec3Expected(output.VN[tris0.vn[2]], 0,1,1, "Tri[0] vn[2] points to wrong vertex normal");
+	// refer to line 37 for formating on tris coordinates
+	CheckVec3Expected(output.V[tris0.v[0]], 0, 0, 0, "Tri[0] v[0] points to wrong vertex");
+	CheckVec3Expected(output.V[tris0.v[1]], 0, 0, 1, "Tri[0] v[1] points to wrong vertex");
+	CheckVec3Expected(output.V[tris0.v[2]], 0, 1, 0, "Tri[0] v[2] points to wrong vertex");
+	CheckVec3Expected(output.VN[tris0.vn[0]], 0, 0, 1, "Tri[0] vn[0] points to wrong vertex normal");
+	CheckVec3Expected(output.VN[tris0.vn[1]], 1, 0, 1, "Tri[0] vn[1] points to wrong vertex normal");
+	CheckVec3Expected(output.VN[tris0.vn[2]], 0, 1, 1, "Tri[0] vn[2] points to wrong vertex normal");
 
-
-    return succeed;
-
+	return succeed;
 }
 
 int main()
