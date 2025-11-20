89ed6abd0f2fd33e0aa2a0d2a27ba74f00015c2e
reagan <rjgron01@louisville.edu>
2025-11-20 21:28:27 +0000

    Fixed pathological.h

--- inc/pathological.h
+++ inc/pathological.h
@@ -90,4 +90,12 @@ class Pathological
 
 	// TODO: Scene, obj, etc loaders
 	static bool load_default_scene(Scene& sc, Camera& cam);
+	Scene& get_scene()
+	{
+		return sc;
+	}
+	Camera& get_camera()
+	{
+		return cam;
+	}
 };
